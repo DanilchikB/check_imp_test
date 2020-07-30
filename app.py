39697bd6f5b753c4bb62956e502573d461c1a482
DanilchikB <danil1357997@gmail.com>
2020-07-30 21:48:31 +0300

    Implemented the main idea of the application: find unused imported objects

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print('Hello')
+from sys import argv
+
+file_name = ''
+finded_import_objects=set()
+
+if len(argv) > 1:
+    file_name = argv[1]
+    
+
+with open(file_name) as f:
+    for line in f:
+        if 'import ' in line:
+            import_objects = ''.join(line.strip().split('import')[1:]).replace(' ', '')
+            finded_import_objects.update(import_objects.split(','))
+        else:
+            for import_object in finded_import_objects.copy():
+                if import_object in line:
+                    finded_import_objects.discard(import_object)
+
+print('Imported objects are not used: ', str(finded_import_objects))
+
---
+++ test.py
@@ -0,0 +1,7 @@
+import os
+from sys import argv, api_version
+
+import_as = ''
+
+print('a')
+print(argv)
